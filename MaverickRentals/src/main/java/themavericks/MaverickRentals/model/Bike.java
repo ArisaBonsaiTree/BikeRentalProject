1445001ac3f822167b01dbcbbea03ad212b7baba
ToanDNN <toann1998@gmail.com>
2023-03-21 12:44:47 -0500

    Added getter and setter methods
    Added the getter and setter methods to the model classes

--- MaverickRentals/src/main/java/themavericks/MaverickRentals/model/Bike.java
+++ MaverickRentals/src/main/java/themavericks/MaverickRentals/model/Bike.java
@@ -1,4 +1,38 @@
 package themavericks.MaverickRentals.model;
 
+import java.util.Objects;
+
 public class Bike {
+    private int bikeId;
+
+    public String getBikeType() {
+        return bikeType;
+    }
+
+    public void setBikeType(String bikeType) {
+        this.bikeType = bikeType;
+    }
+
+    private String bikeType;
+
+    public int getBikeId() {
+        return bikeId;
+    }
+
+    public void setBikeId(int bikeId) {
+        this.bikeId = bikeId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Bike bike = (Bike) o;
+        return bikeId == bike.bikeId && Objects.equals(bikeType, bike.bikeType);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bikeId, bikeType);
+    }
 }
--- MaverickRentals/src/main/java/themavericks/MaverickRentals/model/Reservation.java
+++ MaverickRentals/src/main/java/themavericks/MaverickRentals/model/Reservation.java
@@ -1,4 +1,84 @@
 package themavericks.MaverickRentals.model;
 
+import java.math.BigDecimal;
+import java.time.LocalTime;
+import java.util.Objects;
+
 public class Reservation {
+    private int reservationId;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private int startStationId;
+    private int endStationId;
+    private BigDecimal price;
+    private String customerName;
+
+    public int getReservationId() {
+        return reservationId;
+    }
+
+    public void setReservationId(int reservationId) {
+        this.reservationId = reservationId;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public int getStartStationId() {
+        return startStationId;
+    }
+
+    public void setStartStationId(int startStationId) {
+        this.startStationId = startStationId;
+    }
+
+    public int getEndStationId() {
+        return endStationId;
+    }
+
+    public void setEndStationId(int endStationId) {
+        this.endStationId = endStationId;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Reservation that = (Reservation) o;
+        return reservationId == that.reservationId && startStationId == that.startStationId && endStationId == that.endStationId && Objects.equals(startTime, that.startTime) && Objects.equals(endTime, that.endTime) && Objects.equals(price, that.price) && Objects.equals(customerName, that.customerName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(reservationId, startTime, endTime, startStationId, endStationId, price, customerName);
+    }
 }
