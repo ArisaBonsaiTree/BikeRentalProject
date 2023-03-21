71b93660fa1da695e5b6161efce1a98302dbaa41
ToanDNN <toann1998@gmail.com>
2023-03-21 13:13:30 -0500

    Updated models
    Updated the bike and reservation model classes

--- MaverickRentals/src/main/java/themavericks/MaverickRentals/model/Bike.java
+++ MaverickRentals/src/main/java/themavericks/MaverickRentals/model/Bike.java
@@ -1,9 +1,13 @@
 package themavericks.MaverickRentals.model;
 
+import java.math.BigDecimal;
 import java.util.Objects;
 
 public class Bike {
     private int bikeId;
+    private String bikeType;
+    private BigDecimal bikePrice;
+    private boolean available;
 
     public String getBikeType() {
         return bikeType;
@@ -13,8 +17,6 @@ public class Bike {
         this.bikeType = bikeType;
     }
 
-    private String bikeType;
-
     public int getBikeId() {
         return bikeId;
     }
@@ -23,16 +25,29 @@ public class Bike {
         this.bikeId = bikeId;
     }
 
+    public void setBikePrice(BigDecimal bikePrice) {
+        this.bikePrice = bikePrice;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Bike bike = (Bike) o;
-        return bikeId == bike.bikeId && Objects.equals(bikeType, bike.bikeType);
+        return bikeId == bike.bikeId && available == bike.available && Objects.equals(bikeType, bike.bikeType) && Objects.equals(bikePrice, bike.bikePrice);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(bikeId, bikeType);
+        return Objects.hash(bikeId, bikeType, bikePrice, available);
     }
 }
--- MaverickRentals/src/main/java/themavericks/MaverickRentals/model/Reservation.java
+++ MaverickRentals/src/main/java/themavericks/MaverickRentals/model/Reservation.java
@@ -6,6 +6,7 @@ import java.util.Objects;
 
 public class Reservation {
     private int reservationId;
+    private int bikeId;
     private LocalTime startTime;
     private LocalTime endTime;
     private int startStationId;
@@ -69,16 +70,25 @@ public class Reservation {
         this.customerName = customerName;
     }
 
+
+    public int getBikeId() {
+        return bikeId;
+    }
+
+    public void setBikeId(int bikeId) {
+        this.bikeId = bikeId;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Reservation that = (Reservation) o;
-        return reservationId == that.reservationId && startStationId == that.startStationId && endStationId == that.endStationId && Objects.equals(startTime, that.startTime) && Objects.equals(endTime, that.endTime) && Objects.equals(price, that.price) && Objects.equals(customerName, that.customerName);
+        return reservationId == that.reservationId && bikeId == that.bikeId && startStationId == that.startStationId && endStationId == that.endStationId && Objects.equals(startTime, that.startTime) && Objects.equals(endTime, that.endTime) && Objects.equals(price, that.price) && Objects.equals(customerName, that.customerName);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(reservationId, startTime, endTime, startStationId, endStationId, price, customerName);
+        return Objects.hash(reservationId, bikeId, startTime, endTime, startStationId, endStationId, price, customerName);
     }
 }
