c66dc23ec731a599e263860dc63d52e497d0c73a
ArisaBonsaiTree <angelevarela@hotmail.com>
2023-03-23 11:23:23 -0700

    Finally works and got it to render properly

---
+++ frontend/src/EditReservation.js
@@ -0,0 +1,13 @@
+// EditReservation.jsx
+import React from 'react';
+
+function EditReservation() {
+  return (
+    <div>
+      <h1>Edit Reservation</h1>
+      <p>This is the edit reservation page.</p>
+    </div>
+  );
+}
+
+export default EditReservation;
